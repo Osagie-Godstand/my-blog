665026bb61a4e79e5ab491599dde1f44667f0658
Osagie-Godstand <51753700+Osagie-Godstand@users.noreply.github.com>
2023-11-14 23:57:54 +0000

    feat: ci/cd updated template text

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -1,36 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var isDarkMode = document.body.classList.contains("dark-mode");
-
+    var isDarkMode = localStorage.getItem("darkMode") === "true";
+    var element = document.body;
     var sunIcon = document.querySelector("#darkModeToggle .fa-sun");
     var moonIcon = document.querySelector("#darkModeToggle .fa-moon");
 
     if (isDarkMode) {
-        sunIcon.style.display = "none"; 
-        moonIcon.style.display = "inline";
-    } else {
+        element.classList.add("dark-mode");
         sunIcon.style.display = "inline";
-        moonIcon.style.display = "none"; 
+        moonIcon.style.display = "none";
+        sunIcon.style.color = "#fff";
+        moonIcon.style.color = "#fff";
+    } else {
+        sunIcon.style.display = "none";
+        moonIcon.style.display = "inline";
+        sunIcon.style.color = "#000";
+        moonIcon.style.color = "#000";
     }
 });
 
 function myFunction() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
-
     var sunIcon = document.querySelector("#darkModeToggle .fa-sun");
     var moonIcon = document.querySelector("#darkModeToggle .fa-moon");
 
+    element.classList.toggle("dark-mode");
     var isDarkMode = element.classList.contains("dark-mode");
 
+    localStorage.setItem("darkMode", isDarkMode);
+
     if (isDarkMode) {
-        sunIcon.style.display = "none";
-        moonIcon.style.display = "inline"; 
-        sunIcon.style.color = "#fff"; 
+        sunIcon.style.display = "inline";
+        moonIcon.style.display = "none";
+        sunIcon.style.color = "#fff";
         moonIcon.style.color = "#fff";
     } else {
-        sunIcon.style.display = "inline"; 
-        moonIcon.style.display = "none";
-        sunIcon.style.color = "#000"; 
+        sunIcon.style.display = "none";
+        moonIcon.style.display = "inline";
+        sunIcon.style.color = "#000";
         moonIcon.style.color = "#000";
     }
 }
