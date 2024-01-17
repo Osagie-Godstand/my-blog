df5e10d4d43c801169fa644e14249a61c518efe4
Osagie-Godstand <51753700+Osagie-Godstand@users.noreply.github.com>
2024-01-17 22:41:27 +0000

    feat: CI/CD new blog

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -30,6 +30,8 @@ func main() {
 
 	r.Get("/blog1", blog1Handler)
 
+	r.Get("/blog2", blog2Handler)
+
 	fmt.Println("Server is running on :8080")
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
@@ -67,6 +69,10 @@ func project4Handler(w http.ResponseWriter, r *http.Request) {
 
 func blog1Handler(w http.ResponseWriter, r *http.Request) {
 	serveFile(w, r, "tmpl/blog/blog1/blog1.tmpl")
+}
+
+func blog2Handler(w http.ResponseWriter, r *http.Request) {
+	serveFile(w, r, "tmpl/blog/blog2/blog2.tmpl")
 }
 
 func serveFile(w http.ResponseWriter, r *http.Request, filePath string) {
